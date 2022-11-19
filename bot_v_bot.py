1c6b134f19e9a0072a69eb656eb4f17e6031bf45
cmitchellng <80634268+cmitchellng@users.noreply.github.com>
2022-11-18 19:53:54 -0600

    initial

---
+++ bot_v_bot.py
@@ -0,0 +1,25 @@
+from agent import naive
+import goboard
+import gotypes
+from utils import print_board, print_move
+import time
+
+def main():
+  board_size = 9
+  bots = {
+    gotypes.Player.black: naive.RandomBot(),
+    gotypes.Player.white: naive.RandomBot(),
+  }
+  game = goboard.GameState.new_game(board_size)
+
+  while not game.is_over():
+      time.sleep(1.0)
+      print(chr(27) + "[2J")
+      print_board(game.board)
+      print()
+      bot_move = bots[game.next_player].select_move(game)
+      print_move(game.next_player, bot_move)
+      game = game.apply_move(bot_move)
+
+if __name__ == '__main__':
+  main()
